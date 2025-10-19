577f6556af0e916cece89a67e7d656134368e5f7
ayidhalzahrani <ucapalz@ucl.ac.uk>
2025-10-19 15:48:13 +0100

    Investigating the strongest earthquacke in UK

--- earthquakes.py
+++ earthquakes.py
@@ -1,61 +1,54 @@
-# The Python standard library includes some functionality for communicating
-# over the Internet.
-# However, we will use a more powerful and simpler library called requests.
-# This is external library that you may need to install first.
-import requests
 
+import requests
+import json
+import numpy as np
 
 def get_data():
-    # With requests, we can ask the web service for the data.
-    # Can you understand the parameters we are passing here?
     response = requests.get(
         "http://earthquake.usgs.gov/fdsnws/event/1/query.geojson",
-        params={
-            'starttime': "2000-01-01",
+    params ={
+            'starttime': "2014-01-01",
             "maxlatitude": "58.723",
             "minlatitude": "50.008",
             "maxlongitude": "1.67",
             "minlongitude": "-9.756",
             "minmagnitude": "1",
-            "endtime": "2018-10-11",
+            "endtime": "2025-10-11",
             "orderby": "time-asc"}
     )
-
-    # The response we get back is an object with several fields.
-    # The actual contents we care about are in its text field:
     text = response.text
-    # To understand the structure of this text, you may want to save it
-    # to a file and open it in VS Code or a browser.
-    # See the README file for more information.
-    ...
-
-    # We need to interpret the text to get values that we can work with.
-    # What format is the text in? How can we load the values?
-    return ...
+    return json.loads(text)
 
 def count_earthquakes(data):
-    """Get the total number of earthquakes in the response."""
-    return ...
+    # The number of earthquakes is the number of features in the GeoJSON
+    return len(data["features"])
 
 
-def get_magnitude(earthquake):
-    """Retrive the magnitude of an earthquake item."""
-    return ...
+def get_magnitude(data):
+  magnitudes = [feature['properties']['mag'] for feature in data['features']]
+  return magnitudes
+
+def get_location(data):
+    # Return a list of all locations
+    locations = [feature['properties']['place'] for feature in data['features']]
+    return locations
 
 
-def get_location(earthquake):
-    """Retrieve the latitude and longitude of an earthquake item."""
-    # There are three coordinates, but we don't care about the third (altitude)
-    return ...
+def get_maximum(earthquake):
+    mag_max = 0
+    for feature in earthquake['features']:
+        mag = feature['properties']['mag']
+        if mag > mag_max:
+            mag_max = mag
+            location = feature['properties']['place']
+            
+    return mag_max, location
 
 
-def get_maximum(data):
-    """Get the magnitude and location of the strongest earthquake in the data."""
-    ...
 
+earthquake = get_data()
 
-# With all the above functions defined, we can now call them and get the result
-data = get_data()
-print(f"Loaded {count_earthquakes(data)}")
-max_magnitude, max_location = get_maximum(data)
-print(f"The strongest earthquake was at {max_location} with magnitude {max_magnitude}")
+print(count_earthquakes(earthquake))
+print(get_magnitude(earthquake))
+print(get_location(earthquake))
+print(get_maximum(earthquake))
